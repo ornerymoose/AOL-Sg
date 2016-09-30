d1e90e6be8b6116f038f6f591093010ce319e4ce
ornerymoose <dpardy87@gmail.com>
2016-09-30 12:04:27 -0400

    added email templates and updated controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,16 +1,21 @@
 class CustomersController < ApplicationController
+	def home
+	end
 
 	def pb
-		@customer_name = ENV["pb_customers_name"]
-		@cust_email = ENV["pb_customers_email"]
+		@customer_name = ENV["pb_customers_name"].split(",")
+		@cust_email = ENV["pb_customers_email"].split(",")
 
 		@name = ""
 		combined = @cust_email.zip(@customer_name)
 		combined.each do |group|
 			email = group[0]
 			@name = group[1]
-			#UserNotifier.send_pb_email(email, @name).deliver_now
+			UserNotifier.send_pb_email(email, @name).deliver_now
 		end
+	end
+
+	def pb_send
 	end
 
 	def non_pb
@@ -24,5 +29,8 @@ class CustomersController < ApplicationController
 			@name = group[1]
 			#UserNotifier.send_non_pb_email(email, @name).deliver_now
 		end
+	end
+
+	def non_pb_send
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'customers/pb'
-  get 'customers/non_pb'
+	root 'customers#home'
+	get '/pb_send', to: 'customers#pb_send'
+	get '/non_pb_send', to: 'customers#non_pb_send'
+  	get :pb, to: 'customers#pb', as: :pb
+  	get :non_pb, to: 'customers#non_pb', as: :non_pb
 end
