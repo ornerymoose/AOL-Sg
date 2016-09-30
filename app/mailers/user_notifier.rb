990e54351e49c5eea85ca2c4d7eafd9b96ed10b4
ornerymoose <dpardy87@gmail.com>
2016-09-30 09:20:44 -0400

    added email helper and mailers

---
+++ app/mailers/user_notifier.rb
@@ -0,0 +1,13 @@
+class UserNotifier < ApplicationMailer
+	default :from => ENV["SENDGRID_EMAIL_FROM"]
+
+	def send_non_pb_email(customer_email, customer_name)
+		@name = customer_name
+    	mail(:to => customer_email, :subject => "Your Summit Broadband Bill")
+  	end  	
+
+  	def send_pb_email(customer_email, customer_name)
+		@name = customer_name
+    	mail(:to => customer_email, :subject => "Your Summit Broadband Bill")
+  	end  	
+end
