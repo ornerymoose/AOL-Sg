47bf22890a8c6813a74631de11b15e89d1af5edf
ornerymoose <dpardy87@gmail.com>
2017-02-03 09:14:20 -0500

    took out unncessary code from customers controller, put backup email template in gitignore for now

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,40 +3,21 @@ class CustomersController < ApplicationController
 	end
 
 	def pb
-		@customer_name = ENV["pb_customers_name"].split(",")
-		@cust_email = ENV["pb_customers_email"].split(",")
-
-		@name = "CUSTOMER"
-		combined = @cust_email.zip(@customer_name)
-		combined.each do |group|
-			email = group[0]
-			@name = group[1]
-			#UserNotifier.send_pb_email(email, @name).deliver_now
+		ENV["pb_customers_email"].split(",").each do |email|
+			#UserNotifier.send_pb_email(email).deliver_now
 		end
 	end
 
-	def pb_send
-	end
-
 	def non_pb
-		@customer_name = ENV["non_pb_customers_name"].split(",")
-		@cust_email = ENV["non_pb_customers_email"].split(",")
-
-		combined = @cust_email.zip(@customer_name)
-		@name = "CUSTOMER"
-		combined.each do |group|
-			email = group[0]
-			#UserNotifier.send_non_pb_email(email, @name).deliver_now
+		ENV["non_pb_customers_email"].split(",").each do |email|
+			UserNotifier.send_non_pb_email(email).deliver_now
 		end
 	end
 
 	def dais
 		ENV["dais_customers_email"].split(",").each do |email|		
-			UserNotifier.send_dais_email(email).deliver_now
+			#UserNotifier.send_dais_email(email).deliver_now
 		end
-	end
-
-	def dais_send
 	end
 
 end
