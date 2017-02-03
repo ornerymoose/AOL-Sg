4b7f7a03cae1aa2d4ec2941e4f891340d6b86805
ornerymoose <dpardy87@gmail.com>
2017-02-03 09:23:07 -0500

    fixed intendation, looked awful on github

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,13 +1,13 @@
 class UserNotifier < ApplicationMailer
-	default :from => ENV["SENDGRID_EMAIL_FROM"]
+  default :from => ENV["SENDGRID_EMAIL_FROM"]
 
-	def send_non_pb_email(customer_email)
+  def send_non_pb_email(customer_email)
     mail(:to => customer_email, :subject => "Your Summit Broadband Bill")
-  end  	
+  end
 
-	def send_pb_email(customer_email)
+  def send_pb_email(customer_email)
     mail(:to => customer_email, :subject => "Your Summit Broadband Bill")
-	end  	
+  end
 
   def send_dais_email(customer_email)
     mail(:to => customer_email, :subject => "Your DAIS Bill")
