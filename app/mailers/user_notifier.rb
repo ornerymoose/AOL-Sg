57a3de82306bcf5fa57ca393bc26d15a688b0445
ornerymoose <dpardy87@gmail.com>
2017-02-03 09:21:04 -0500

    removed unncessary params from user_notifier methods

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,17 +1,15 @@
 class UserNotifier < ApplicationMailer
 	default :from => ENV["SENDGRID_EMAIL_FROM"]
 
-	def send_non_pb_email(customer_email, customer_name)
-		@name = customer_name
-    	mail(:to => customer_email, :subject => "Your Summit Broadband Bill")
-  	end  	
+	def send_non_pb_email(customer_email)
+    mail(:to => customer_email, :subject => "Your Summit Broadband Bill")
+  end  	
 
-  	def send_pb_email(customer_email, customer_name)
-		@name = customer_name
-    	mail(:to => customer_email, :subject => "Your Summit Broadband Bill")
-  	end  	
+	def send_pb_email(customer_email)
+    mail(:to => customer_email, :subject => "Your Summit Broadband Bill")
+	end  	
 
-  	def send_dais_email(customer_email)
-    	mail(:to => customer_email, :subject => "Your DAIS Bill")
-  	end  	
+  def send_dais_email(customer_email)
+    mail(:to => customer_email, :subject => "Your DAIS Bill")
+  end  	
 end
