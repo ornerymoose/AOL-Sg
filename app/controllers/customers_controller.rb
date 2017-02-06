e907a333b2357c7cd6f91977f6e226821135cd8e
ornerymoose <dpardy87@gmail.com>
2017-02-06 13:49:29 -0500

    fixed spacing for customers controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,20 +1,19 @@
 class CustomersController < ApplicationController
-
   include CustomersHelper
 
-	def home
-	end
+  def home
+  end
 
-	def pb
-		send_pb_email("pb_esp.csv")
-	end
+  def pb
+    send_pb_email("pb_esp.csv")
+  end
 
-	def non_pb
+  def non_pb
     send_summit_email("sum_esp.csv")
-	end
+  end
 
-	def dais
-		send_dais_email("dais_esp.csv")
-	end
-
+  def dais
+    send_dais_email("dais_esp.csv")
+  end
+  
 end
